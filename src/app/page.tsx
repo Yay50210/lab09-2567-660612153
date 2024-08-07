4789388a8cea8c2cf4305cdc3e9018a7a38d6622
Best <Photchara_k@cmu.ac.th>
2024-08-07 15:15:10 +0700

    alittle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 import Task from "@components/Task";
@@ -10,34 +12,43 @@ export default function Home() {
   // Define the interface of task-item object
   interface TaskItem {
     // your code here
+    id:string;
+    title: string;
+    completed: boolean;
   }
 
   // useState hook for an array of task-item objects
   const [tasks, setTasks] = useState<TaskItem[]>([]);
 
   // Define the function with proper type
-  const addTask = (newTaskTitle) => {
+  const addTask = (newTaskTitle:string) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
   };
 
   // Define the function with proper type
-  const deleteTask = (taskId) => {
+  const deleteTask = (taskId:string) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   };
 
   // Define the function with proper type
-  const toggleDoneTask = (taskId) => {
+  const toggleDoneTask = (taskId:string) => {
     //structuredClone will copy an array or an object "deeply"
     //So objects within an object will be copied too
     const newTasks = structuredClone(tasks);
     //search for a task based on condition
     const task = newTasks.find((x) => x.id === taskId);
-    task.completed = !task.completed;
+    if(task) {task.completed = !task.completed;}
     setTasks(newTasks);
   };
+  const Alltask = () => {
+    return tasks.filter((tasks) => tasks.id).length;
+}
+const comptasks = () => {
+  return tasks.filter((tasks) => tasks.completed).length;
+}
 
   return (
     // Main container
@@ -48,7 +59,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({Alltask()}) Done ({comptasks()})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -67,7 +78,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2024" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2024" fullName="Photchara Khantijai" studentId="660612153" />
     </div>
   );
-}
+};
